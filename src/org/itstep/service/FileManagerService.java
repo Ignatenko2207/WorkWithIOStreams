615891fa4344c0b753d092d73096e70222cb9d05
Alex <ignatenko2207@gmail.com>
2019-04-11 21:07:38 +0300

    Add test

--- src/org/itstep/service/FileManagerService.java
+++ src/org/itstep/service/FileManagerService.java
@@ -20,54 +20,40 @@ import org.itstep.entity.MySimpleObject;
 
 public class FileManagerService {
 
-	private final static String SERIAL_FILE_PATH = System.getProperty("user.dir") 
-													+ System.getProperty("file.separator")
-													+ "files"
-													+ System.getProperty("file.separator")
-													+ "my-object.obj";
-	
-	private final static String DIR_PATH = System.getProperty("user.dir") 
-													+ System.getProperty("file.separator")
-													+ "files"
-													+ System.getProperty("file.separator");
-	
-	private final static String TEXT_FILE_PATH = System.getProperty("user.dir") 
-													+ System.getProperty("file.separator")
-													+ "files"
-													+ System.getProperty("file.separator")
-													+ "test.txt";
-	
-	
-	
+	private final static String SERIAL_FILE_PATH = System.getProperty("user.dir") + System.getProperty("file.separator")
+			+ "files" + System.getProperty("file.separator") + "my-object.obj";
+
+	private final static String DIR_PATH = System.getProperty("user.dir") + System.getProperty("file.separator")
+			+ "files" + System.getProperty("file.separator");
+
+	private final static String TEXT_FILE_PATH = System.getProperty("user.dir") + System.getProperty("file.separator")
+			+ "files" + System.getProperty("file.separator") + "test.txt";
+
 	// Serialization
 	public static void writeObjectToFile(MySimpleObject myObject) {
-		try (
-				FileOutputStream fos = new FileOutputStream(SERIAL_FILE_PATH);
-				ObjectOutputStream oos = new ObjectOutputStream(fos);
-			){
-			
+		try (FileOutputStream fos = new FileOutputStream(SERIAL_FILE_PATH);
+				ObjectOutputStream oos = new ObjectOutputStream(fos);) {
+
 			oos.writeObject(myObject);
-			oos.flush();		
+			oos.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static MySimpleObject readObjectFromFile() {
 		MySimpleObject myObject = null;
-		try (
-				FileInputStream fis = new FileInputStream(SERIAL_FILE_PATH);
-				ObjectInputStream ois = new ObjectInputStream(fis);
-			){
+		try (FileInputStream fis = new FileInputStream(SERIAL_FILE_PATH);
+				ObjectInputStream ois = new ObjectInputStream(fis);) {
 			myObject = (MySimpleObject) ois.readObject();
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 		return myObject;
 	}
-	
+
 	// bytes
-	
+
 	private static byte[] readBytesFromFile(String fileName) {
 		String filePath = DIR_PATH + fileName;
 		byte[] bytes = null;
@@ -78,66 +64,53 @@ public class FileManagerService {
 		}
 		return bytes;
 	}
-	
+
 	private static void writeBytesToDir(String fileName, byte[] bytes) {
-		
-		try (
-				FileOutputStream fos = new FileOutputStream(DIR_PATH + fileName);
-			){
+
+		try (FileOutputStream fos = new FileOutputStream(DIR_PATH + fileName);) {
 			fos.write(bytes);
 			fos.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void copyFile(String original, String copy) {
 		byte[] bytes = readBytesFromFile(original);
 		writeBytesToDir(copy, bytes);
 	}
-	
+
 	// text
-	
+
 	public static void writeTextToFile(String text, boolean append) {
-		try (FileWriter fileWriter = new FileWriter(TEXT_FILE_PATH, append)){
+		try (FileWriter fileWriter = new FileWriter(TEXT_FILE_PATH, append)) {
 			fileWriter.write(text + "\n");
 			fileWriter.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static List<ConnectionEntity> readConnectionsInfoFromFile(){
+
+	public static List<ConnectionEntity> readConnectionsInfoFromFile() {
 		List<ConnectionEntity> connectionEntities = new ArrayList<>();
-		
-		try (
-				Reader fileReader = new FileReader(TEXT_FILE_PATH);
-				BufferedReader bufferedReader = new BufferedReader(fileReader);
-			){
+
+		try (Reader fileReader = new FileReader(TEXT_FILE_PATH);
+				BufferedReader bufferedReader = new BufferedReader(fileReader);) {
 			String line;
 			while ((line = bufferedReader.readLine()) != null) {
 				String[] words = line.split(" ");
 				Long time = Long.parseLong(words[0]);
 				Integer sessionId = Integer.parseInt(words[1]);
-				String ip = words[2];				
+				String ip = words[2];
 				ConnectionEntity connectionEntity = new ConnectionEntity(time, sessionId, ip);
 				connectionEntities.add(connectionEntity);
 			}
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return connectionEntities;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
